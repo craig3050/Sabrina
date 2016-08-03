555574cb0fb0ec4a2d9eac77a78672895d08f47f
Craig Cuninghame <craig3050@gmail.com>
2016-08-03 22:30:28 +0100

    Working display, added default display before displaying messages
    This will show message, time, date and IP when the last message was
    recieved, not a live update of time.

--- LCDDisplay/Screen_Output.py
+++ LCDDisplay/Screen_Output.py
@@ -4,8 +4,10 @@ from time import *
 from datetime import datetime
 from subprocess import *
 import paho.mqtt.client as mqtt
+import re #regex module for character searching
 
 MQTT_Message = ""
+MQTT_Topic = ""
 
 while True:
    
@@ -18,15 +20,27 @@ while True:
       # Subscribing in on_connect() means that if we lose the connection and
       # reconnect then subscriptions will be renewed.
       print ("Subscribing to all")
-      client.subscribe("$SYS/#")
+      client.subscribe("#")
 
 
    # The callback for when a PUBLISH message is received from the server.
    def on_message(client, userdata, msg):
       print ("MQTT Message incoming")
-      MQTT_Message = (msg.topic+" "+str(msg.payload))
-      print (MQTT_Message)
+      MQTT_Topic = (msg.topic+" "+str(msg.payload))
+      print (MQTT_Topic)
 
+      #Regex expression to group the topic and message in to separate sections - keeping the last topic value after the last "/"
+      #e.g "/test1/test2/temperature 666" - group 2 becomes "temperature 666" while group 1 becomes /test1/test2/
+      text_remove = re.search(r'(.*\/)([^/]*)', MQTT_Topic)
+      print (text_remove.group(2))
+
+      MQTT_Message = MQTT_Topic.replace(text_remove.group(1), "")
+      MQTT_Topic = MQTT_Topic.replace(text_remove.group(2), "")
+      print (MQTT_Message)
+      print (MQTT_Topic)
+
+
+      # get IP address from Pi
       cmd = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1"
      
       # lcd start
@@ -63,8 +77,8 @@ while True:
       print("Printing info to screen")
       lcd.lcd_display_string('%s' % (the_time), 1)
       lcd.lcd_display_string('IP %s' % ( ip_number ), 2)
-      lcd.lcd_display_string("%s" % (MQTT_Message), 3)
-      lcd.lcd_display_string("              ", 4)
+      lcd.lcd_display_string("%s" % (MQTT_Topic), 3)
+      lcd.lcd_display_string("%s" % (MQTT_Message), 4)
 
       print("Sleeping")
       sleep(20)
@@ -76,7 +90,48 @@ while True:
    client.on_message = on_message
 
    client.connect("192.168.0.10", 1883, 60)
-   
+
+   # get IP address from Pi
+   cmd = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+     
+   # lcd start
+   print ("Starting LCD")
+   lcd = lcddriver.lcd()
+
+   # clears the lcd screen
+   print ("Clearing LCD")
+   lcd.lcd_clear()
+
+   # function to get the ip address (and a lot of other junk)
+   print ("Getting IP Address")
+   def run_cmd(cmd):
+      p = Popen(cmd, shell=True, stdout=PIPE)
+      output = p.communicate()[0]
+      return output
+
+   # runs function to get ip address and cleans up the output
+   print ("Clearing Up IP address")
+   ipaddr = run_cmd(cmd)
+   ip_number = ""
+   for x in ipaddr:
+      if x == "\n":
+         break
+      else:
+         ip_number += x
+
+
+   # gets the current time & date
+   print ("Getting time and date")
+   the_time = strftime('%Y-%m-%d     %H:%M')
+
+   #   print the information to the screen
+   print("Printing info to screen")
+   lcd.lcd_display_string('%s' % (the_time), 1)
+   lcd.lcd_display_string('IP %s' % ( ip_number ), 2)
+   lcd.lcd_display_string("Sabrina Home Auto", 3)
+   lcd.lcd_display_string("Craig Rules!", 4)
+
+   sleep(20)
 
    # Blocking call that processes network traffic, dispatches callbacks and
    # handles reconnecting.
